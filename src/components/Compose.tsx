ffdd7f5f6902a9d8043d669fdf7542294301e278
AkiraFukushima <h3.poteto@gmail.com>
2022-11-24 22:00:45 +0900

    refs #3 Post status

--- src/components/Compose.tsx
+++ src/components/Compose.tsx
@@ -1,12 +1,13 @@
-import { Container, Header, Content, FlexboxGrid, Button, Dropdown, Avatar } from 'rsuite'
+import { Container, Header, Content, FlexboxGrid, Button, Dropdown, Avatar, Form, Input, ButtonToolbar, Schema } from 'rsuite'
 import { Icon } from '@rsuite/icons'
 import { BsX } from 'react-icons/bs'
-import { forwardRef, useEffect, useState } from 'react'
+import { useEffect, useState, forwardRef, useRef } from 'react'
 import { invoke } from '@tauri-apps/api/tauri'
 
 import { Server } from 'src/entities/server'
 import { Account } from 'src/entities/account'
 import failoverImg from 'src/utils/failoverImg'
+import generator, { detector } from 'megalodon'
 
 const renderAccountIcon = (props, ref, account: [Account, Server] | undefined) => {
   if (account && account.length > 0) {
@@ -32,13 +33,34 @@ const renderAccountIcon = (props, ref, account: [Account, Server] | undefined) =
   }
 }
 
+const Textarea = forwardRef<HTMLTextAreaElement>((props, ref) => <Input {...props} as="textarea" ref={ref} />)
+
+const post = async (account: Account, server: Server, value: FormValue) => {
+  const sns = await detector(server.base_url)
+  const client = generator(sns, server.base_url, account.access_token, 'Fedistar')
+  const res = await client.postStatus(value.status)
+  return res
+}
+
 type Props = {
   setOpened: (value: boolean) => void
+}
+
+type FormValue = {
+  status: string
 }
 
 const Compose: React.FC<Props> = props => {
   const [accounts, setAccounts] = useState<Array<[Account, Server]>>([])
   const [fromAccount, setFromAccount] = useState<[Account, Server]>()
+  const [formValue, setFormValue] = useState<FormValue>({
+    status: ''
+  })
+  const formRef = useRef<any>()
+
+  const model = Schema.Model({
+    status: Schema.Types.StringType().isRequired('This field is required.')
+  })
 
   useEffect(() => {
     const f = async () => {
@@ -52,6 +74,24 @@ const Compose: React.FC<Props> = props => {
   const selectAccount = (eventKey: string) => {
     const account = accounts[parseInt(eventKey)]
     setFromAccount(account)
+  }
+
+  const handleSubmit = async () => {
+    if (formRef === undefined || formRef.current === undefined) {
+      return
+    } else if (!formRef.current.check()) {
+      console.error('Validation Error')
+      return
+    } else {
+      await post(fromAccount[0], fromAccount[1], formValue)
+      clear()
+    }
+  }
+
+  const clear = () => {
+    setFormValue({
+      status: ''
+    })
   }
 
   return (
@@ -79,6 +119,19 @@ const Compose: React.FC<Props> = props => {
             </Dropdown>
           </FlexboxGrid.Item>
         </FlexboxGrid>
+        <div style={{ fontSize: '1.2em', padding: '12px 0' }}>Status</div>
+        <Form fluid model={model} ref={formRef} onChange={setFormValue} formValue={formValue}>
+          <Form.Group controlId="status">
+            <Form.Control rows={5} name="status" accepter={Textarea} />
+          </Form.Group>
+          <Form.Group>
+            <ButtonToolbar style={{ textAlign: 'right' }}>
+              <Button appearance="primary" type="submit" onClick={handleSubmit}>
+                Post
+              </Button>
+            </ButtonToolbar>
+          </Form.Group>
+        </Form>
       </Content>
     </Container>
   )
