a9ed02a2891795d7bf030b65591d0726f9290d8c
AkiraFukushima <h3.poteto@gmail.com>
2022-12-01 21:06:58 +0900

    refs #95 Display only tag for video and audio

--- src/components/timelines/status/Attachments.tsx
+++ src/components/timelines/status/Attachments.tsx
@@ -1,5 +1,8 @@
 import Image from 'next/image'
 import { Entity } from 'megalodon'
+import { Tag } from 'rsuite'
+import { Icon } from '@rsuite/icons'
+import { BsCameraVideo, BsVolumeUp } from 'react-icons/bs'
 
 type Props = {
   attachments: Array<Entity.Attachment>
@@ -11,18 +14,55 @@ const Attachments: React.FC<Props> = props => {
     <div style={{ display: 'flex', flexWrap: 'wrap' }}>
       {props.attachments.map((media, index) => (
         <div key={index} style={{ margin: '4px' }}>
-          <Image
-            width={128}
-            height={128}
-            src={media.preview_url}
-            alt={media.description ? media.description : media.id}
-            onClick={() => props.openMedia(media)}
-            style={{ objectFit: 'cover' }}
-          />
+          <Attachment media={media} openMedia={props.openMedia} />
         </div>
       ))}
     </div>
   )
+}
+
+type AttachmentProps = {
+  media: Entity.Attachment
+  openMedia: (media: Entity.Attachment) => void
+}
+
+const Attachment: React.FC<AttachmentProps> = props => {
+  const { media } = props
+
+  switch (media.type) {
+    case 'gifv':
+      return (
+        <Tag>
+          <Icon as={BsCameraVideo} style={{ fontSize: '1.2em', paddingRight: '4px' }} />
+          GIF
+        </Tag>
+      )
+    case 'video':
+      return (
+        <Tag>
+          <Icon as={BsCameraVideo} style={{ fontSize: '1.2em', paddingRight: '4px' }} />
+          VIDEO
+        </Tag>
+      )
+    case 'audio':
+      return (
+        <Tag>
+          <Icon as={BsVolumeUp} style={{ fontSize: '1.2em', paddingRight: '4px' }} />
+          AUDIO
+        </Tag>
+      )
+    default:
+      return (
+        <Image
+          width={128}
+          height={128}
+          src={media.preview_url}
+          alt={media.description ? media.description : media.id}
+          onClick={() => props.openMedia(media)}
+          style={{ objectFit: 'cover' }}
+        />
+      )
+  }
 }
 
 export default Attachments
