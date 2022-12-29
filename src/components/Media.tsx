30c9dbdae486b2c45367df4311ac634238ea888a
AkiraFukushima <h3.poteto@gmail.com>
2022-12-30 01:20:39 +0900

    refs #95 Handle audio and video as media

--- src/components/Media.tsx
+++ src/components/Media.tsx
@@ -1,6 +1,7 @@
 import { Modal } from 'rsuite'
 import Image from 'next/image'
 import { Entity } from 'megalodon'
+import { ReactElement } from 'react'
 
 type Props = {
   media: Entity.Attachment | null
@@ -10,20 +11,25 @@ type Props = {
 
 const Media: React.FC<Props> = props => {
   return (
-    <Modal open={props.opened} size="lg" onClose={props.close}>
+    <Modal open={props.opened} size="lg" onClose={props.close} style={{ height: 'calc(100% - 30px)' }} dialogClassName="media-dialog">
       <Modal.Header></Modal.Header>
-      <Modal.Body style={{ height: '600px' }}>
-        {props.media && (
-          <Image
-            fill
-            src={props.media?.url}
-            alt={props.media?.description ? props.media?.description : props.media?.id}
-            style={{ objectFit: 'contain' }}
-          />
-        )}
+      <Modal.Body style={{ height: '100%', textAlign: 'center', display: 'flex', flexDirection: 'column', justifyContent: 'center' }}>
+        {props.media && mediaComponent(props.media)}
       </Modal.Body>
     </Modal>
   )
+}
+
+const mediaComponent = (media: Entity.Attachment): ReactElement => {
+  switch (media.type) {
+    case 'gifv':
+      return <video src={media.url} autoPlay loop style={{ objectFit: 'contain' }} />
+    case 'video':
+    case 'audio':
+      return <video src={media.url} autoPlay loop controls style={{ objectFit: 'contain' }} />
+    default:
+      return <Image fill src={media.url} alt={media.description ? media.description : media.id} style={{ objectFit: 'contain' }} />
+  }
 }
 
 export default Media
