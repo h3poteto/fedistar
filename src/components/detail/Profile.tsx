ba694059c47b465bf5158e3e2340c5a17b50593b
AkiraFukushima <h3.poteto@gmail.com>
2023-01-25 23:21:04 +0900

    Add back button in detail drawer

--- src/components/detail/Profile.tsx
+++ src/components/detail/Profile.tsx
@@ -1,8 +1,8 @@
 import { open } from '@tauri-apps/api/shell'
 import { invoke } from '@tauri-apps/api/tauri'
 import generator, { Entity, MegalodonInterface } from 'megalodon'
-import { BsX, BsThreeDotsVertical } from 'react-icons/bs'
-import { Button, Container, Content, Dropdown, FlexboxGrid, Header, IconButton, Popover, useToaster, Whisper } from 'rsuite'
+import { BsThreeDotsVertical } from 'react-icons/bs'
+import { Button, Content, Dropdown, FlexboxGrid, IconButton, Popover, useToaster, Whisper } from 'rsuite'
 import { Icon } from '@rsuite/icons'
 import Image from 'next/image'
 import emojify from 'src/utils/emojify'
@@ -56,10 +56,6 @@ const Profile: React.FC = () => {
     setRelationship(res.data)
   }
 
-  const close = () => {
-    router.push({ query: {} })
-  }
-
   const followButton = () => {
     if (!relationship) {
       return null
@@ -102,81 +98,74 @@ const Profile: React.FC = () => {
   }, [client, account])
 
   return (
-    <Container className="profile" style={{ height: '100%', borderLeft: '1px solid var(--rs-gray-600)' }}>
-      <Header style={{ backgroundColor: 'var(--rs-gray-700)' }}>
-        <Button appearance="link" onClick={close}>
-          <Icon as={BsX} style={{ fontSize: '1.4em' }} />
-        </Button>
-      </Header>
-      {user && (
-        <Content style={{ height: '100%', backgroundColor: 'var(--rs-gray-800)' }}>
-          <div className="profile-header-image" style={{ width: '100%', backgroundColor: 'var(--rs-body)' }}>
-            <img src={user.header} alt="header image" style={{ objectFit: 'cover', width: '100%', height: '146px' }} />
+    user && (
+      <Content style={{ height: '100%', backgroundColor: 'var(--rs-gray-800)' }}>
+        <div className="profile-header-image" style={{ width: '100%', backgroundColor: 'var(--rs-body)' }}>
+          <img src={user.header} alt="header image" style={{ objectFit: 'cover', width: '100%', height: '146px' }} />
+        </div>
+        <div className="profile-header-body" style={{ padding: '0 20px' }}>
+          <FlexboxGrid justify="space-between" align="bottom" style={{ marginTop: '-50px' }}>
+            <FlexboxGrid.Item>
+              <Image src={user.avatar} alt={user.acct} width={94} height={94} style={{ borderRadius: '4px' }} />
+            </FlexboxGrid.Item>
+            <FlexboxGrid.Item>
+              <FlexboxGrid style={{ gap: '8px' }}>
+                <FlexboxGrid.Item>{followButton()}</FlexboxGrid.Item>
+                <FlexboxGrid.Item>
+                  <Whisper
+                    placement="bottomEnd"
+                    controlId="control-id-profile-detail"
+                    trigger="click"
+                    speaker={({ className, left, top, onClose }, ref) =>
+                      profileMenu(
+                        {
+                          className,
+                          left,
+                          top,
+                          onClose,
+                          client,
+                          user,
+                          relationship,
+                          onChange: () => {
+                            loadRelationship(client, user)
+                          }
+                        },
+                        ref
+                      )
+                    }
+                  >
+                    <IconButton icon={<Icon as={BsThreeDotsVertical} />} />
+                  </Whisper>
+                </FlexboxGrid.Item>
+              </FlexboxGrid>
+            </FlexboxGrid.Item>
+          </FlexboxGrid>
+          <div className="username" style={{ margin: '16px 0' }}>
+            <span
+              style={{ fontSize: '1.2em', fontWeight: 'bold', display: 'block' }}
+              dangerouslySetInnerHTML={{ __html: emojify(user.display_name, user.emojis) }}
+            ></span>
+            <span style={{ display: 'block', color: 'var(--rs-text-secondary)' }}>@{user.acct}</span>
           </div>
-          <div className="profile-header-body" style={{ padding: '0 20px' }}>
-            <FlexboxGrid justify="space-between" align="bottom" style={{ marginTop: '-50px' }}>
-              <FlexboxGrid.Item>
-                <Image src={user.avatar} alt={user.acct} width={94} height={94} style={{ borderRadius: '4px' }} />
-              </FlexboxGrid.Item>
-              <FlexboxGrid.Item>
-                <FlexboxGrid style={{ gap: '8px' }}>
-                  <FlexboxGrid.Item>{followButton()}</FlexboxGrid.Item>
-                  <FlexboxGrid.Item>
-                    <Whisper
-                      placement="bottomEnd"
-                      controlId="control-id-profile-detail"
-                      trigger="click"
-                      speaker={({ className, left, top, onClose }, ref) =>
-                        profileMenu(
-                          {
-                            className,
-                            left,
-                            top,
-                            onClose,
-                            client,
-                            user,
-                            relationship,
-                            onChange: () => {
-                              loadRelationship(client, user)
-                            }
-                          },
-                          ref
-                        )
-                      }
-                    >
-                      <IconButton icon={<Icon as={BsThreeDotsVertical} />} />
-                    </Whisper>
-                  </FlexboxGrid.Item>
-                </FlexboxGrid>
-              </FlexboxGrid.Item>
-            </FlexboxGrid>
-            <div className="username" style={{ margin: '16px 0' }}>
-              <span
-                style={{ fontSize: '1.2em', fontWeight: 'bold', display: 'block' }}
-                dangerouslySetInnerHTML={{ __html: emojify(user.display_name, user.emojis) }}
-              ></span>
-              <span style={{ display: 'block', color: 'var(--rs-text-secondary)' }}>@{user.acct}</span>
-            </div>
-            <div className="bio">
-              <div dangerouslySetInnerHTML={{ __html: user.note }} />
-            </div>
-            <div className="fields" style={{ backgroundColor: 'var(--rs-body)', borderRadius: '4px', margin: '16px 0' }}>
-              {user.fields.map((data, index) => (
-                <dl key={index} style={{ padding: '8px 16px', margin: 0, borderBottom: '1px solid var(--rs-bg-card)' }}>
-                  <dt>{data.name}</dt>
-                  <dd dangerouslySetInnerHTML={{ __html: emojify(data.value, user.emojis) }} style={{ margin: 0 }} />
-                </dl>
-              ))}
-            </div>
-            <FlexboxGrid className="status" justify="space-between">
-              <FlexboxGrid.Item style={{ fontWeight: 'bold' }}>{user.statuses_count} Posts</FlexboxGrid.Item>
-              <FlexboxGrid.Item style={{ fontWeight: 'bold' }}>{user.following_count} Following</FlexboxGrid.Item>
-              <FlexboxGrid.Item style={{ fontWeight: 'bold' }}>{user.followers_count} Followers</FlexboxGrid.Item>
-            </FlexboxGrid>
+          <div className="bio">
+            <div dangerouslySetInnerHTML={{ __html: user.note }} />
           </div>
-        </Content>
-      )}
-    </Container>
+          <div className="fields" style={{ backgroundColor: 'var(--rs-body)', borderRadius: '4px', margin: '16px 0' }}>
+            {user.fields.map((data, index) => (
+              <dl key={index} style={{ padding: '8px 16px', margin: 0, borderBottom: '1px solid var(--rs-bg-card)' }}>
+                <dt>{data.name}</dt>
+                <dd dangerouslySetInnerHTML={{ __html: emojify(data.value, user.emojis) }} style={{ margin: 0 }} />
+              </dl>
+            ))}
+          </div>
+          <FlexboxGrid className="status" justify="space-between">
+            <FlexboxGrid.Item style={{ fontWeight: 'bold' }}>{user.statuses_count} Posts</FlexboxGrid.Item>
+            <FlexboxGrid.Item style={{ fontWeight: 'bold' }}>{user.following_count} Following</FlexboxGrid.Item>
+            <FlexboxGrid.Item style={{ fontWeight: 'bold' }}>{user.followers_count} Followers</FlexboxGrid.Item>
+          </FlexboxGrid>
+        </div>
+      </Content>
+    )
   )
 }
 
