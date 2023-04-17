64cc643d67043060dacf9a5e840bff2c9d12c705
AkiraFukushima <h3.poteto@gmail.com>
2023-04-18 00:57:20 +0900

    Save frontend error logs in backend

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,9 +5,21 @@ import '../style.css'
 import '../App.scss'
 
 import '../i18n'
+import { invoke } from '@tauri-apps/api/tauri'
+import { useEffect } from 'react'
 
 // This default export is required in a new `pages/_app.js` file.
 export default function MyApp({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    window.addEventListener('unhandledrejection', (event: PromiseRejectionEvent) => {
+      invoke('frontend_log', { level: 'error', message: event.reason.toString() })
+    })
+    window.addEventListener('error', (event: ErrorEvent) => {
+      console.log(event)
+      invoke('frontend_log', { level: 'error', message: event.message.toString() })
+    })
+  }, [])
+
   return (
     <CustomProvider theme="dark">
       <Component {...pageProps} />
