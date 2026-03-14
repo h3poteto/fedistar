b7aef9dd8ee9afd00cc029591a8a7d0479b0ec84
AkiraFukushima <h3.poteto@gmail.com>
2026-03-14 20:41:04 +0900

    refs #2100 Use X11 for all Linux to prevent custom title bar in Wayland

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -4,5 +4,14 @@
 )]
 
 fn main() {
+    // https://github.com/h3poteto/fedistar/issues/2100
+    // https://github.com/tauri-apps/tao/issues/1046
+    // https://github.com/tauri-apps/tauri/issues/14251
+    // In Wayland, tauri generates custom title bar. So, this collide with the desktop generated title bar.
+    // For workaround, use X11 temproarily.
+    if cfg!(target_os = "linux") {
+        std::env::set_var("GDK_BACKEND", "x11");
+        std::env::set_var("WEBKIT_DISABLE_DMABUF_RENDERER", "1");
+    }
     let _ = fedistar::run();
 }
